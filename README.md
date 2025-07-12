6be7021b1773ece8a3492c8675e077de70b25df6
Stewie Nguyen <mopro1010@gmail.com>
2025-07-12 10:32:43 +0700

    Init test unit

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 
 This is the final project for CS50 Python.
 
-## What is this?
+## Video Demo
+
+
+## Description
 
 This is the recreation of the classic chilren's game "Hangman" with extra features such as definitions and synonyms to create an educational, yet entertaining, experience.
 
--- test_hangman.py
+++ test_hangman.py
@@ -1,5 +1,26 @@
 from hangman import Hangman
 import hangman
+import pytest
+import json
 
 def test_get_word_meaning():
-    assert hangman.get_word_meaning("") == "hey"
+    with open("filtered.json", "r") as dic:
+        dictionary = json.load(dic)
+    assert hangman.get_word_meaning("assert") == dictionary["ASSERT"]["MEANINGS"][0]
+
+def test_get_word_synonym():
+    with open("filtered.json", "r") as dic:
+        dictionary = json.load(dic)
+    assert hangman.get_word_synonym("assert") == dictionary["ASSERT"]["SYNONYMS"]
+
+def test_get_random_word():
+    with open("filtered.json", "r") as dic:
+        dictionary = json.load(dic)
+    random_word = hangman.get_random_word()
+
+    assert random_word.upper() in dictionary
+
+def test_get_letter_position():
+    x = [(0, "a"), (1, "s"), (2, "s"), (3, "e"), (4, "r"), (5, "t")]
+    y = ["a", "s", "s", "e", "r", "t"]
+    assert hangman.get_letter_position("assert") == (x, y)
